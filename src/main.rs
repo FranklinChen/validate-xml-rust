27fd621719604e96ff320eccbf4acd8c1c1fa8ce
Franklin Chen <franklinchen@franklinchen.com>
2023-01-10 15:19:44 -0500

    Clean up concurrency. Do not cache Schemas to files, but just keep in memory.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,54 @@
-#[macro_use]
-extern crate lazy_static;
-
-extern crate dirs;
-extern crate docopt;
-extern crate ignore;
-extern crate libc;
-extern crate parking_lot;
-extern crate rayon;
-extern crate regex;
-extern crate reqwest;
-extern crate serde;
-
+use regex::Regex;
+use std::path::{Path, PathBuf};
+use tokio::io::{BufReader, AsyncBufReadExt};
+use tokio::task;
+use tokio::fs::File;
+use tokio::runtime::Handle;
+use lazy_static::lazy_static;
+// TODO use clap
 use docopt::Docopt;
 use serde::Deserialize;
+use std::ffi::CString;
+use cached::proc_macro::cached;
 
-const USAGE: &'static str = "
+/// For libxml2 FFI.
+use libc::{c_char, c_int, c_uint, FILE};
+
+/// Fake opaque structs from C libxml2.
+pub enum XmlSchema {}
+pub enum XmlSchemaParserCtxt {}
+pub enum XmlSchemaValidCtxt {}
+
+/// We know that libxml2 schema data structure is [thread-safe](http://xmlsoft.org/threads.hml).
+#[derive(Clone, Copy)]
+struct XmlSchemaPtr(pub *mut XmlSchema);
+
+unsafe impl Send for XmlSchemaPtr {}
+unsafe impl Sync for XmlSchemaPtr {}
+
+#[link(name = "xml2")]
+extern "C" {
+    pub fn xmlInitParser();
+    pub fn xmlInitGlobals();
+
+    // xmlschemas
+    pub fn xmlSchemaNewMemParserCtxt(buffer: *const c_char, size: c_int) -> *mut XmlSchemaParserCtxt;
+    //pub fn xmlSchemaSetParserErrors();
+    pub fn xmlSchemaParse(ctxt: *const XmlSchemaParserCtxt) -> *mut XmlSchema;
+    pub fn xmlSchemaFreeParserCtxt(ctxt: *mut XmlSchemaParserCtxt);
+    pub fn xmlSchemaDump(output: *mut FILE, schema: *const XmlSchema);
+    pub fn xmlSchemaFree(schema: *mut XmlSchema);
+    pub fn xmlSchemaNewValidCtxt(schema: *const XmlSchema) -> *mut XmlSchemaValidCtxt;
+    pub fn xmlSchemaFreeValidCtxt(ctxt: *mut XmlSchemaValidCtxt);
+    //pub fn xmlSchemaSetValidErrors();
+    pub fn xmlSchemaValidateFile(
+        ctxt: *const XmlSchemaValidCtxt,
+        file_name: *const c_char,
+        options: c_uint,
+    ) -> c_int;
+}
+
+const USAGE: &str = "
 Validate XML files concurrently and downloading remote XML Schemas only once.
 
 Usage:
@@ -34,111 +68,41 @@ struct Args {
     arg_dir: String,
 }
 
-use ignore::Walk;
-use parking_lot::RwLock;
-use regex::Regex;
-use reqwest::blocking::Client;
-use std::collections::HashMap;
-use std::ffi::CString;
-use std::fs;
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
-use std::path::{Path, PathBuf};
-
-/// For libxml2 FFI.
-use libc::{c_char, c_int, c_uint, FILE};
-
-/// Fake opaque structs from C libxml2.
-pub enum XmlSchema {}
-pub enum XmlSchemaParserCtxt {}
-pub enum XmlSchemaValidCtxt {}
-
-/// We know that libxml2 schema data structure is [thread-safe](http://xmlsoft.org/threads.html).
-#[derive(Clone, Copy)]
-struct XmlSchemaPtr(pub *mut XmlSchema);
-
-unsafe impl Send for XmlSchemaPtr {}
-unsafe impl Sync for XmlSchemaPtr {}
-
-#[link(name = "xml2")]
-extern "C" {
-    pub fn xmlInitParser();
-    pub fn xmlInitGlobals();
-
-    // xmlschemas
-    pub fn xmlSchemaNewParserCtxt(url: *const c_char) -> *mut XmlSchemaParserCtxt;
-    //pub fn xmlSchemaSetParserErrors();
-    pub fn xmlSchemaParse(ctxt: *const XmlSchemaParserCtxt) -> *mut XmlSchema;
-    pub fn xmlSchemaFreeParserCtxt(ctxt: *mut XmlSchemaParserCtxt);
-    pub fn xmlSchemaDump(output: *mut FILE, schema: *const XmlSchema);
-    pub fn xmlSchemaFree(schema: *mut XmlSchema);
-    pub fn xmlSchemaNewValidCtxt(schema: *const XmlSchema) -> *mut XmlSchemaValidCtxt;
-    pub fn xmlSchemaFreeValidCtxt(ctxt: *mut XmlSchemaValidCtxt);
-    //pub fn xmlSchemaSetValidErrors();
-    pub fn xmlSchemaValidateFile(
-        ctxt: *const XmlSchemaValidCtxt,
-        file_name: *const c_char,
-        options: c_uint,
-    ) -> c_int;
-}
-
-fn extract_schema_url(path: &Path) -> String {
+/// Return the first Schema URL found, if any.
+/// Panic on any I/O error.
+async fn extract_schema_url(path: &Path) -> Option<String> {
     lazy_static! {
         static ref RE: Regex = Regex::new(r#"xsi:schemaLocation="\S+\s+(.+?)""#)
             .expect("failed to compile schemaLocation regex");
     }
 
-    let f = File::open(path).unwrap();
-    for line in BufReader::new(f).lines() {
-        if let Some(caps) = RE.captures(&line.unwrap()) {
-            return caps[1].to_string();
+    let file = File::open(path).await.unwrap();
+    let mut lines = BufReader::new(file).lines();
+    while let Some(line) = lines.next_line().await.unwrap() {
+        if let Some(caps) = RE.captures(&line) {
+            return Some(caps[1].to_owned());
         }
     }
-    unreachable!()
+    None
 }
 
-/// Cache into ~/.xmlschemas/ directory.
-fn download_schema(url: &str) -> XmlSchemaPtr {
+/// Cache schema into memory after downloading from Web once and stashing into memory.
+///
+/// Panics on I/O error.
+#[cached(sync_writes = true)]
+async fn get_schema(url: String) -> XmlSchemaPtr {
     lazy_static! {
-        static ref CLIENT: Client = Client::new();
-        static ref SCHEMA_DIR: PathBuf = {
-            let home_dir = dirs::home_dir().expect("could not find home directory");
-            let schema_dir = home_dir.join(".xmlschemas");
-
-            fs::create_dir_all(schema_dir.as_path())
-                .expect(&format!("could not create {}", schema_dir.display()));
-            schema_dir
-        };
+        static ref CLIENT: reqwest::Client = reqwest::Client::new();
     }
 
-    let encoded_file_name = url.replace("/", "%2F");
-    let file_path_buf = SCHEMA_DIR.join(encoded_file_name);
-    let file_path = file_path_buf.as_path();
+    // DEBUG to show that download happens only once.
+    println!("Downloading now {url}...");
 
-    if file_path.is_file() {
-        // Already cached. (Hopefully not trash.)
-    } else {
-        // Synchronously download from Web to local file.
-        // TODO Make async?
-
-        // DEBUG to show that download happens only once.
-        println!("Downloading now {}...", url);
-
-        let mut response = CLIENT.get(url).send().unwrap();
-        let mut new_file = File::create(file_path).expect(&format!(
-            "could not create cache file {}",
-            file_path.display()
-        ));
-        let mut buf = Vec::new();
-        response.read_to_end(&mut buf).expect("read_to_end failed");
-        new_file.write_all(&buf).expect("write_all failed");
-    }
-
-    let c_url = CString::new(file_path.to_str().unwrap()).unwrap();
+    let response = CLIENT.get(url.as_str()).send().await.unwrap().bytes().await.unwrap();
 
     unsafe {
-        let schema_parser_ctxt = xmlSchemaNewParserCtxt(c_url.as_ptr());
+        let schema_parser_ctxt = xmlSchemaNewMemParserCtxt(response.as_ptr() as *const c_char,
+                                                           response.len() as i32);
 
         // Use default callbacks rather than overriding.
         //xmlSchemaSetParserErrors();
@@ -150,36 +114,12 @@ fn download_schema(url: &str) -> XmlSchemaPtr {
     }
 }
 
-/// Download an XML Schema file only if necessary.
-/// Many threads could be trying.
-fn get_schema(url: String) -> XmlSchemaPtr {
-    lazy_static! {
-        static ref M: RwLock<HashMap<String, XmlSchemaPtr>> = RwLock::new(HashMap::new());
-    }
-
-    {
-        let m = M.read();
-        m.get(&url).map(|&s| s)
-    }
-    .unwrap_or_else(|| {
-        let mut m = M.write();
-
-        // Double-checked locking pattern.
-        m.get(&url).map(|&s| s).unwrap_or_else(|| {
-            // TODO make async?
-            let schema = download_schema(&url);
-            m.insert(url, schema);
-            schema
-        })
-    })
-}
-
 /// Copy the behavior of [`xmllint`](https://github.com/GNOME/libxml2/blob/master/xmllint.c)
-fn validate(path: &Path) {
-    let url = extract_schema_url(path);
-    let schema = get_schema(url);
+async fn validate(path_buf: PathBuf) {
+    let url = extract_schema_url(path_buf.as_path()).await.unwrap();
+    let schema = get_schema(url).await;
 
-    let path_str = path.to_str().unwrap();
+    let path_str = path_buf.to_str().unwrap();
     let c_path = CString::new(path_str).unwrap();
 
     unsafe {
@@ -188,26 +128,26 @@ fn validate(path: &Path) {
 
         // TODO better error message with integrated path using callback.
         //xmlSchemaSetValidErrors();
+
+        // This reads the file and validates it.
         let result = xmlSchemaValidateFile(schema_valid_ctxt, c_path.as_ptr(), 0);
         if result == 0 {
-            writeln!(std::io::stderr(), "{} validates", path_str).unwrap();
+            eprintln!("{path_str} validates");
         } else if result > 0 {
             // Note: the message is output after the validation messages.
-            writeln!(std::io::stderr(), "{} fails to validate", path_str).unwrap();
+            eprintln!("{path_str} fails to validate");
         } else {
-            writeln!(
-                std::io::stderr(),
-                "{} validation generated an internal error",
-                path_str
-            )
-            .unwrap();
+            eprintln!(
+                "{path_str} validation generated an internal error"
+            );
         }
 
         xmlSchemaFreeValidCtxt(schema_valid_ctxt);
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args: Args = Docopt::new(USAGE)
         .and_then(|d| d.deserialize())
         .unwrap_or_else(|e| e.exit());
@@ -218,18 +158,19 @@ fn main() {
         xmlInitGlobals();
     }
 
-    rayon::scope(|scope| {
-        for result in Walk::new(&args.arg_dir) {
-            scope.spawn(move |_| {
-                if let Ok(entry) = result {
-                    let path = entry.path();
-                    if let Some(extension) = path.extension() {
-                        if extension.to_str().unwrap() == extension_str {
-                            validate(path);
-                        }
-                    }
+    // No real point in using WalkParallel.
+    for result in ignore::Walk::new(&args.arg_dir) {
+        if let Ok(entry) = result {
+            let path = entry.path().to_owned();
+            if let Some(extension) = path.extension() {
+                if extension.to_str().unwrap() == extension_str {
+                    task::block_in_place(move || {
+                        Handle::current().block_on(async move {
+                            validate(path).await;
+                        })
+                    })
                 }
-            });
+            }
         }
-    });
+    }
 }
